53109d7432d8355a2e8885b150b4e609ab83e770
Jose Maria Gayosso <josemaria@hashed.io>
2023-03-27 14:08:36 -0600

    El tlaloc no sabee lo que dice XD ta loco

--- track2.sh
+++ track2.sh
@@ -1,31 +1,25 @@
 # #!/bin/bash
 
-# # Buscar todos los directorios que sean repositorios de git en el directorio actual
-# for dir in $(find ./../ -name ".git" -type d -exec dirname {} \;); do
-#     # Obtener el nombre del repositorio
-#     repo_name=$(basename "$dir")
-#     # Mostrar el nombre del repositorio
-#     echo "=================== $repo_name ==================="
-#     # Mostrar los últimos commits
-#     git -C "$dir" log -n 5 --pretty=format:'%h - %an, %ar : %s'
-#     echo
-# done
-
-#!/bin/bash
-
 # Obtener el nombre de usuario del usuario actual
-user_name='Jose Maria Gayosso'
+# user_name='Jose Maria Gayosso'
+user_name=$(git config user.name )
 
 # Buscar todos los directorios que sean repositorios de git en el directorio actual
 for dir in $(find ./../ -name ".git" -type d -exec dirname {} \;); do
     # Obtener el nombre del repositorio
     repo_name=$(basename "$dir")
-    # Mostrar el nombre del repositorio
+    # Mostrar el nombre del repositorio‧
     echo "=================== $repo_name ==================="
     # Mostrar los últimos commits del usuario actual del día actual
-    # git -C "$dir" log --since=midnight --author="$user_name" --pretty=format:'%h - %an, %ar : %s'
-    # git -C "$dir" log --author="$user_name" --since=midnight --oneline --pretty=format:'%s'
-    commit_log=$(git -C "$dir" log --author="$user_name" --since=midnight --oneline --pretty=format:'%s')
+    commit_log=$(git -C "$dir" log --author="$user_name" --since=midnight --oneline --pretty=format:'‧ %s')
     echo $commit_log
+    # curl https://api.openai.com/v1/completions \
+    # -H "Content-Type: application/json" \
+    # -H "Authorization: Bearer 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" \
+    # -d '{
+    # "model": "text-davinci-003",
+    # "prompt": "Please do this tasks list in a proffesional format to log: ($commit_log)",
+    # "temperature": 0, "max_tokens": 2000
+    # }'
     echo
 done
