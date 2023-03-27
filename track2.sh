a412e41ff268315b1bc8e3db04a848f235506458
tlacloc <desneruda@gmail.com>
2023-03-27 15:05:53 -0600

    update output and add a variable to define where the parent directories are

--- track2.sh
+++ track2.sh
@@ -1,25 +1,21 @@
 # #!/bin/bash
 
 # Obtener el nombre de usuario del usuario actual
-user_name=$(git config user.name )
+user_name=$(git config user.name)
+echo '[' > "commits.json"
 
-$token = $1
-# Buscar todos los directorios que sean repositorios de git en el directorio actual
-for dir in $(find ./../ -name ".git" -type d -exec dirname {} \;); do
-    # Obtener el nombre del repositorio
-    repo_name=$(basename "$dir")
-    # Mostrar el nombre del repositorio‧
-    echo "=================== $repo_name ==================="
-    # Mostrar los últimos commits del usuario actual del día actual
-    commit_log=$(git -C "$dir" log --author="$user_name" --since=midnight --oneline --pretty=format:'‧ %s')
-    echo $commit_log
-    # curl https://api.openai.com/v1/completions \
-    # -H "Content-Type: application/json" \
-    # -H "Authorization: Bearer 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" \
-    # -d '{
-    # "model": "text-davinci-003",
-    # "prompt": "Please do this tasks list in a proffesional format to log: ($commit_log)",
-    # "temperature": 0, "max_tokens": 2000
-    # }'
-    echo
-done
+while IFS= read -r line
+do
+    # Itera sobre cada hijo del directorio actual para acceder a los repositorios
+    for d in "$line"/*; do
+        # Mostrar el nombre del repositorio
+        repo_name=$(basename -s .git $(git --git-dir=$d/.git remote get-url origin))
+        # Mostrar los últimos commits del usuario actual del día actual
+        commit_log=$(git -C "$d" log --author="$user_name" --since=midnight --oneline --pretty=format:'‧ %s')
+        if [ -n "$commit_log" ]; then
+            echo { \"$repo_name\" : \"$commit_log\" },
+        fi
+    done 
+done < "repos.txt" >> "commits.json"
+
+echo ']' >> "commits.json"
